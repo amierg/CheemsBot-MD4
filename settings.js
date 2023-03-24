f72b5d67e6c47928dd3d655976e1bc4babdc6d34
amierg <128812653+amierg@users.noreply.github.com>
2023-03-24 14:43:44 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['9660540637980'] //ur owner number
-global.ownername = "ABDUL RAHMAN🐼🍷 ٫" //ur owner name
-global.ytname = "YT: ABDUL RAHMAN" //ur yt chanel name
-global.socialm = "GitHub: ABDUL RAHMAN" //ur github or insta name
+global.vcardowner = ['201026975194'] //ur owner number
+global.ownername = "𝑬𝑺𝑳𝑨𝑴✹⃝‌꙰ 🐼🍷 ٫" //ur owner name
+global.ytname = "YT: 𝑬𝑺𝑳𝑨𝑴✹⃝‌꙰" //ur yt chanel name
+global.socialm = "GitHub: 𝑬𝑺𝑳𝑨𝑴✹⃝‌꙰" //ur github or insta name
 global.location = "Egypt, Cairo, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['9660540637980']
-global.ownertag = '9660540637980' //ur tag number
-global.botname = 'BOT ABDUL RAHMAN🐼🍷 ٫' //ur bot name
-global.linkz = "https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv" //your theme url which will be displayed on whatsapp
-global.websitex = "https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv" //ur website to be displayed
-global.botscript = 'https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv' //script link
+global.owner = ['201026975194']
+global.ownertag = '201026975194' //ur tag number
+global.botname = '𝑬𝑺𝑳𝑨𝑴✹⃝‌꙰ ٫' //ur bot name
+global.linkz = "https://chat.whatsapp.com/Co2PlX9fK65ErCJeZQyVmb" //your theme url which will be displayed on whatsapp
+global.websitex = "https: https://chat.whatsapp.com/Co2PlX9fK65ErCJeZQyVmb" //ur website to be displayed
+global.botscript = 'https://chat.whatsapp.com/Co2PlX9fK65ErCJeZQyVmb' //script link
 global.reactmoji = "🌚" //ur menu react emoji
 global.themeemoji = "🌞" //ur theme emoji
-global.packname = "⍣عبدالرحمن⍣" //ur sticker watermark packname
-global.author = "⍣9660540637980⍣" //ur sticker watermark author
+global.packname = "⍣ 𝑬𝑺𝑳𝑨𝑴✹⃝‌꙰ ⍣" //ur sticker watermark packname
+global.author = "⍣201026975194⍣" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['9660540637980'] //ur premium numbers
+global.premium = ['201026975194'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
